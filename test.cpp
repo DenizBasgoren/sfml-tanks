f831e3f7dbf5894376848b95c542628cdda82ffa
circuitmaster <45884610+circuitmaster@users.noreply.github.com>
2020-03-01 21:41:49 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
     std::cout << "Developers:" << std::endl;
     std::cout << "deniz" << std::endl;
+    cout << "Yuppi we dont have to type std anymore :)" << endl; 
+
 
     return 0;
 }
