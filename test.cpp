e70dcbd7a17ad2a4e89ae28c72770e163848b911
MrShaTr <56830053+MrShaTr@users.noreply.github.com>
2020-03-02 08:13:00 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -5,13 +5,10 @@ using namespace std;
 int main()
 {
     std::cout << "Developers:" << std::endl;
-<<<<<<< HEAD
     std::cout << "deniz , mahir" << std::endl;
-=======
     std::cout << "deniz" << std::endl;
     cout << "Yuppi we dont have to type std anymore :)" << endl; 
 
->>>>>>> f831e3f7dbf5894376848b95c542628cdda82ffa
 	cout<<"Trying 1 2 3  ...."
 
     return 0;
